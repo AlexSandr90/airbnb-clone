9d5084c52b3b42b4a87cf7b6ae7e53c3eefb26f3
Alex Gaytsuk <gayrsuk@gmail.com>
2023-04-20 00:13:11 +0300

    check user

--- app/components/map/Map.tsx
+++ app/components/map/Map.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import L from 'leaflet';
-import { MapContainer, Marker, TileLayer } from 'react-leaflet'
+import {MapContainer, Marker, TileLayer} from 'react-leaflet'
 
 import 'leaflet/dist/leaflet.css'
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
@@ -24,7 +24,7 @@ interface MapProps {
 const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-const Map: React.FC<MapProps> = ({ center }) => {
+const Map: React.FC<MapProps> = ({center}) => {
     return (
         <MapContainer
             center={center as L.LatLngExpression || [51, -0.09]}
@@ -36,9 +36,9 @@ const Map: React.FC<MapProps> = ({ center }) => {
                 url={url}
                 attribution={attribution}
             />
-            {center && (
-                <Marker position={center as L.LatLngExpression} />
-            )}
+            {center ? (
+                <Marker position={center as L.LatLngExpression}/>
+            ) : null}
         </MapContainer>
     )
 };
